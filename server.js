9bfeef763d50e537c15af8739d168eeda8e911a6
Katie Stapleton <Deepdarkstar7@gmail.com>
2021-01-25 11:38:33 -0600

    nodemon enabled, index.html linked to server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+//ES6 style. 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
 
 app.listen(3000, function() {
     console.log('listening on 3000')
